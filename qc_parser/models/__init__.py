a24b159c4dfea492f1b13064a0a1c1919b900a84
Darian Hole <darian.hole@canada.ca>
2023-07-19 09:33:43 -0500

    function name fix, add back importing to the models

--- qc_parser/models/__init__.py
+++ qc_parser/models/__init__.py
@@ -1,1 +1,13 @@
 '''File models and general functions'''
+from .qc import *
+from .Meta import *
+from .Consensus import *
+from .Alleles import *
+from .PerBaseCoverage import *
+from .Variants import *
+from .Vcf import *
+from .NegativeControl import *
+from .primers import *
+from .Lineage import *
+from .Snpeff import *
+from .WatchList import *
