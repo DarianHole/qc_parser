c1421ec96215b3bc5c0aec5701bda3278e802fd7
Darian Hole <darian.hole@canada.ca>
2023-07-19 10:08:01 -0500

    downgrade python version required

--- setup.py
+++ setup.py
@@ -23,6 +23,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.9',
+    python_requires='>=3.8',
     scripts=['bin/qc_parser']
 )
